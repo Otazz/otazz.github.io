2d83e26be96a64f16d279ad84add68818380d1ce
Otazz <ot.rodrigues@hotmail.com>
2016-06-08 10:05:15 -0300

    Update script.js

--- script.js
+++ script.js
@@ -13,6 +13,10 @@ $(document).ready(function() {
        $(this).toggle(1000);
    });
    $('#badd').click(function(){
-       $('.nl').append('<p>'+$('input[name="add"]').val()+'</p>');
+      $('.nl').append('<p>'+$('input[name="add"]').val()+'</p>');
+      $('.nl').append('<button type=button id=rem>Remove last line</button>');
+   });
+   $('#rem').click(function(){
+      $('.nl').last().remove;
    });
 });
